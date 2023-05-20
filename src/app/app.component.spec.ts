2476c24908e5f3c535acaaae2da61a5b33ad9ccb
SLeBron <sophieclebron@gmail.com>
2023-05-19 19:54:58 -0600

    added cities component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,6 @@
+import { OnInit } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { FormControl } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { OnDestroy } from '@angular/core';
+import { AppComponent } from './app.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-project';
+}
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+})
+export class AppComponent implements OnInit, OnDestroy {
+  cities = ["Denver", "Santa Fe", "New York"]
+
+  cityControl: FormControl;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.cityControl = new FormControl("");
+    this.cityControl.valueChanges.subscribe((value) => {
+      this.router.navigate([value]);
+    });
+  }
+
+  ngOnDestroy() {}
 }
