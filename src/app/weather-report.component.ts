f75ed4a7c37dfdba1f4537c354da7359f0ea6c32
SLeBron <sophieclebron@gmail.com>
2023-06-20 09:49:36 -0600

    udpated weather report component

--- src/app/weather-report.component.ts
+++ src/app/weather-report.component.ts
@@ -1,5 +1,6 @@
 import { OnInit } from "@angular/core";
 import { Observable } from "rxjs";
+import { WeatherService } from "./weather-service";
 
 @Component({
     selector: "app-weather-report",
@@ -8,4 +9,17 @@ import { Observable } from "rxjs";
 })
 export class WeatherReportComponent implements OnInit {
     data$: Observable<any>;
+
+    constructor(
+        private weatherService: WeatherService,
+        private route: ActivatedRoute
+    ) {}
+
+    ngOnInit() {
+        this.data$ = this.route.params.pipe(
+            map((params) => params["locationName"]),
+            filter((name) => !!name),
+            concatMap((name) => this.weatherService.getWeatherForCity(name))
+        );
+    }
 }
